e63917f27cabd318e177a72b3274850cf114b5c4
feden2906 <feden2906@gmail.com>
2022-07-30 21:04:37 +0300

    little update

---
+++ interface/auth/auth interface.ts
@@ -0,0 +1,14 @@
+import { UserRoleEnum, UserStatusEnum } from "../../enum";
+import { IBasic } from "../basic.interface";
+
+export interface IAuth extends IBasic{
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    role: UserRoleEnum;
+    status: UserStatusEnum;
+    createdAt: Date;
+    updatedAt: Date;
+}
---
+++ interface/user/user.interface.ts
@@ -0,0 +1,11 @@
+import { UserRoleEnum, UserStatusEnum } from "../../enum";
+import { IBasic } from "../basic.interface";
+
+export interface IUser extends IBasic {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    role: UserRoleEnum;
+    status: UserStatusEnum;
+}
